76d458187b561d0e8a97398abd469c90f6ba7933
Tridip khan <sintoxalpha001@gmail.com>
2024-04-12 20:52:22 +0530

    add NPM file and Content

---
+++ Module/NPM/Basic_Npm.js
@@ -0,0 +1,20 @@
+// this is a Basic Sever code to check how Package.json file is work.
+
+// Create a server using Node JS.
+
+//1. Import http Library module 
+const http = require('http');
+
+//2. Create Server.
+const server = http.createServer((req,res)=>{
+
+    //Here comes the request.
+    res.end("<h1> This is My coding Server.<\h1>");
+});
+
+//add Port number to listen  on.
+server.listen(8080,()=>{
+    console.log("Server Running at http://localhost:8080/");
+});
+
+
